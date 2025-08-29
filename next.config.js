ae9b07364c563e5188ad82fd3452d4db4e5be879
Fahad <fahadbinhussain001@gmail.com>
2025-08-29 21:53:37 +0600

    fix function runtime validity for vercel

--- next.config.js
+++ next.config.js
@@ -9,6 +9,21 @@ const nextConfig = {
       },
     ],
   },
+  experimental: {
+    serverComponentsExternalPackages: ["@prisma/client", "bcryptjs"],
+  },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.externals.push({
+      "utf-8-validate": "commonjs utf-8-validate",
+      bufferutil: "commonjs bufferutil",
+    });
+    return config;
+  },
+  env: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+  },
 };
 
 module.exports = nextConfig;
